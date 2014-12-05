c04f8cefbab33c17c885d381cda404dada2e8dc7
Jason Scheller <jscheller@eyeviewdigital.com>
2014-12-05 14:22:21 -0500

    deploty

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 scp target/hackathon-gingis-1.0.0-SNAPSHOT-jar-with-dependencies.jar ec2-54-172-207-167.compute-1.amazonaws.com:/home/ubuntu/
-scp restart.sh ec2-54-172-207-167.compute-1.amazonaws.com:/home/ubuntu/
+scp *.sh ec2-54-172-207-167.compute-1.amazonaws.com:/home/ubuntu/
